d003718a1633c0fa075fc67b53fcd00fc4fb000c
Joaquin <joaquin@pixelinmobiliario.com>
2023-05-01 19:59:40 -0300

    creacion de tarjeta

--- resources/views/personajes.blade.php
+++ resources/views/personajes.blade.php
@@ -4,9 +4,16 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+    <title>Rick and Morti</title>
 </head>
 <body>
+    <ul>
+        <li><a href="/">Home</a></li>
+        <li><a href="/personajes">Personajes</a></li>
+        <li><a href="">API Cpanel</a></li>
+        <li><a href="">Otra Prueba</a></li>
+    </ul>
     <table>
         <thead>
             <tr>
@@ -18,13 +25,17 @@
         </thead>
         <tbody>
             @foreach($personajes->results as $personaje)
-                <tr>
-                    <td>{{ $personaje->name }}</td>
-                    <td>{{ $personaje->status }}</td>
-                    <td>{{ $personaje->species }}</td>
-                    <td> <img src="{{ $personaje->image }}" alt=""> </td>
-                </tr>
+  
+                <div class="card">
+                    <img src="{{ $personaje->image }}" alt="Imagen de la tarjeta">
+                    <h2>{{ $personaje->name }}</h2>
+                    <p>{{ $personaje->species }}</p>
+                    <p>{{-- $personaje->next --}} </p>
+                    <p>{{ $personaje->gender }}</p>
+                  </div>
+                  <br>
             @endforeach
+
         </tbody>
     </table>
     
